612c90c2ce332d69bbd8ac864f8015bad0d5d9ea
jujunjuna189 <jujunjuna189@gmail.com>
2022-06-02 16:33:22 +0700

    Initial commit

---
+++ database/migrations/2022_06_02_084259_create_jurnal_penyesuaian.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jurnal_penyesuaian', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_akun');
+            $table->double('nominal');
+            $table->date('tgl');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jurnal_penyesuaian');
+    }
+};
